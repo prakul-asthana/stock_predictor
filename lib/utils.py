b4cf015eb922bcbdbaa7d5b63d82bbfa03e6844e
prakul_asthana <prakul.asthana@gmail.com>
2019-11-18 15:07:35 -0800

    Changed pyplot import

--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib 
